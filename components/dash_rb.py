ae6cc1a926008df9c95a393bec55ec54a2af86f3
Júlio César Biz <juliocesarbiz@hotmail.com>
2023-11-05 22:28:36 -0300

    Versao 1.1

--- components/dash_rb.py
+++ components/dash_rb.py
@@ -1,7 +1,9 @@
 # Import packages
+import os
 import pandas as pd
+import re
 import dash
-from dash import Dash, html,  dcc
+from dash import Dash, html,  dcc, dash_table
 import dash_daq as daq
 from datetime import date, datetime, timedelta
 import dash_bootstrap_components as dbc
@@ -10,15 +12,15 @@ from dash.dependencies import Input, Output, State
 import plotly.express as px
 
 from pymongo import MongoClient
+from dotenv import load_dotenv
+load_dotenv()
 
 from app import app
 
 # Conectar ao banco de dados MongoDB
-uri = "mongodb+srv://martins:LlmN6xS3wBMHTMch@cluster0.he6wly3.mongodb.net/?retryWrites=true&w=majority"
-client = MongoClient(uri)
-database = client['ufsc']
+client = MongoClient(os.environ["uri_banco"])
+database = client[os.environ["Database_Name"]]
 
-Atualizacao_grafico = 0
 uso_cpu = 0
 
 
@@ -56,6 +58,7 @@ layout = dbc.Container([
             html.Div(id='dash-monitoramento', className='dbc'),
             html.Br()
         ]),
+        html.Br(),
         dbc.Col([
             dbc.CardGroup([
                 dbc.Card([
@@ -111,8 +114,88 @@ layout = dbc.Container([
                 )
             ], style={'height': 120})
         ], width=4),
-
     ]),
+    html.Br(),
+    dbc.Row([
+        dbc.Col([
+            dbc.CardGroup([
+                dbc.Card([
+                    html.Legend('Up Time'),
+                    html.H5("10 Dias", id='p-uptime-rb', style={}),
+                ], style={'padding-left': '20px', 'padding-top': '10px'}),
+            ],style={'height':120})
+        ], width=2,),
+        dbc.Col([
+            dbc.CardGroup([
+                dbc.Card([
+                    html.Legend('Modelo'),
+                    html.H5("txt", id='p-board-name', style={}),
+                ], style={'padding-left': '20px', 'padding-top': '10px'}),
+            ],style={'height':120})
+        ], width=2,),
+        dbc.Col([
+            dbc.CardGroup([
+                dbc.Card([           
+                    html.Legend('Arquitetura'),
+                    html.H5("txt", id='p-architecture-name', style={}),
+                ], style={'padding-left': '20px', 'padding-top': '10px'}),
+            ],style={'height':120})
+        ], width=2,),
+        dbc.Col([
+            dbc.CardGroup([
+                dbc.Card([
+                    html.Legend('Atualização'),
+                    html.H5("01/01/2023", id='p-build-time', style={}),
+                ], style={'padding-left': '20px', 'padding-top': '10px'}),
+            ],style={'height':120})
+        ], width=2,),
+       dbc.Col([
+            dbc.CardGroup([   
+                dbc.Card([
+                    html.Legend('Firmware'),
+                    html.H5("01/01/2023", id='p-firmware-version', style={}),
+                ], style={'padding-left': '20px', 'padding-top': '10px'}),
+            ],style={'height':120})
+        ], width=2,),
+        dbc.Col([
+            dbc.CardGroup([   
+                dbc.Card([
+                    html.Legend('Bad Blocks'),
+                    html.H5("0", id='p-bad-blocks', style={}),
+                ], style={'padding-left': '20px', 'padding-top': '10px'}),
+            ],style={'height':120})
+        ], width=2,),
+    ]),
+    
+    html.Br(),
+    dbc.Col(
+        dbc.Card(dcc.Graph(id='graph-ping'), style={'height': '100%', 'padding':'10px' }),  width=12
+    ),
+    html.Br(),
+    dbc.Col([
+       
+        dbc.Card([
+            html.Label("Neighbour Discovery"),
+            html.Br(),
+            html.Div(id='tabela-neighbors', className='dbc'),
+            html.Br(),
+            html.Div([
+                dbc.Button(
+                    "Sobre Neighbour Discovery",
+                    id="collapse-button",
+                    className="mb-3",
+                    color="primary",
+                    n_clicks=0,
+                ),
+                dbc.Collapse(
+                    dbc.Card(dbc.CardBody("Os protocolos Neighbour Discovery nos permitem encontrar dispositivos compatíveis com MNDP (MikroTik Neighbor Discovery Protocol), CDP (Cisco Discovery Protocol) ou LLDP (Link Layer Discovery Protocol) no transmissão da Camada2. Ele pode ser usado para mapear sua rede.")),
+                    id="collapse",
+                    is_open=False,
+                ),
+            ])
+        ],style={'height': '100%', 'padding':'10px' } )
+    ],  width=12),
+
     dbc.Row([
         dcc.Graph(id='graph1')
     ]),
@@ -128,6 +211,8 @@ layout = dbc.Container([
                 multi=True)
         ),
     ]),
+    html.Br(),
+    html.Br(),
     dbc.Row([
         dcc.Interval(
             id='interval-component',
@@ -139,7 +224,11 @@ layout = dbc.Container([
 
 # =========  Callbacks  =========== #
 
-# Atualiza o gráfico de linhas com os dados do banco de dados
+
+##----------------------------------------------------------------
+## Atualiza o gráfico de tráfego 
+##----------------------------------------------------------------
+
 @app.callback(
     Output('graph1', 'figure'),
     [
@@ -156,13 +245,10 @@ def update_line_chart(n, data_nome_interface):
     data = database['rb_monitor_traffic'].find(filtro).sort('data-hora',-1).limit(1500)  # Limitando a 10 registros
 
     df = pd.DataFrame(data)
-    print ("data frame")
+    print ("data frame -----------------------------------------------------")
     print(df)
+    print("\n\n\n\n\n\n")
 
-    global Atualizacao_grafico
-    Atualizacao_grafico += 1
-    print ("atualização numero")
-    print (Atualizacao_grafico)
 
     # Gerando o gráfico de linhas duplas
     fig = px.line(df, x='data-hora', y=['rx-bits-per-second', 'tx-bits-per-second'], title='Grafico 1')
@@ -170,8 +256,86 @@ def update_line_chart(n, data_nome_interface):
 
     return fig
 
+##----------------------------------------------------------------
+## Atualiza o tabela neighbors
+##----------------------------------------------------------------
+@app.callback(
+    Output('tabela-neighbors', 'children'),
+    [
+    Input('interval-component', 'n_intervals')
+    ]
+)
 
-#Lista interfaces
+def tabela_neighbors(n):
+
+    collection = database['rb_neighbors'].find().sort('data-hora',-1).limit(3)  # Limitando a 10 registros
+    df_original = pd.DataFrame(collection).sort_values('mac-address')
+
+    colunas_selecionadas = ['.id','interface','mac-address', 'identity','platform','version','address','uptime', 'board']
+    df = df_original[colunas_selecionadas].copy()
+    #
+    df = df.rename(columns={'.id': 'id'})
+    df = df.rename(columns={'identity': 'Nome'})
+    df = df.rename(columns={'platform': 'platforma'})
+    df = df.rename(columns={'version': 'versão'})
+    df = df.rename(columns={'address': 'Endereço'})
+
+
+    df = df.set_index('id')
+
+    print ("data frame")
+    print(df)
+
+    df = df.fillna('-')
+
+    #df.sort_values(by='data', ascending=False)
+
+    tabela = dash_table.DataTable(df.to_dict('records'), [{"name": i, "id": i} for i in df.columns])
+    
+
+    return tabela
+
+@app.callback(
+    Output("collapse", "is_open"),
+    [Input("collapse-button", "n_clicks")],
+    [State("collapse", "is_open")],
+)
+def toggle_collapse(n, is_open):
+    if n:
+        return not is_open
+    return is_open
+
+
+##----------------------------------------------------------------
+## Atualiza o gráfico de latencia de ping
+##----------------------------------------------------------------
+
+@app.callback(
+    Output('graph-ping', 'figure'),
+    Input('interval-component', 'n_intervals')
+)
+
+def update_graph_ping(n):
+
+    data = database['rb_ping'].find().sort('data-hora',-1).limit(300)  # Limitando a 10 registros
+    #data = database['rb_monitor_traffic'].find(filtro).sort('data-hora',-1).limit(1500) 
+    df = pd.DataFrame(data)
+    print ("data frame")
+    print(df)
+    df['avg-rtt_ms'] = df['avg-rtt'].apply(lambda x: int(re.search(r'(\d+)ms(\d+)us', x).group(1)) + int(re.search(r'(\d+)ms(\d+)us', x).group(2)) / 1000 if re.search(r'(\d+)ms(\d+)us', x) else 0)
+    df['time_ms'] = df['time'].apply(lambda x: int(re.search(r'(\d+)ms(\d+)us', x).group(1)) + int(re.search(r'(\d+)ms(\d+)us', x).group(2)) / 1000 if re.search(r'(\d+)ms(\d+)us', x) else 0)
+
+    # Gerando o gráfico de linhas duplas
+    fig = px.line(df, x='data-hora', y=['avg-rtt_ms','time_ms'], title='Ping Servidores DNS')
+    #fig.update_layout(autotypenumbers='convert types')
+
+    return fig
+
+
+##----------------------------------------------------------------
+## Lista interfaces
+##----------------------------------------------------------------
+
 @app.callback(
     [
         Output("dropdown-nome-interfaces", "options"),
@@ -187,8 +351,10 @@ def populate_dropdown_values(data):
     return ([{"label":x, "value": x} for x in val], val)
 
 
+##----------------------------------------------------------------
+## Graduated_bar_cpu
+##----------------------------------------------------------------
 
-#graduated_bar_cpu
 @app.callback(
     [
         Output("graduated-bar-cpu","value"),
@@ -196,13 +362,21 @@ def populate_dropdown_values(data):
         Output("p-cpu-frequency-dashboard","children"),
         Output("graduated-bar-cpu-temperature","value"),
         Output("p-cpu-temperature-dashboard","children"),
+
+        Output("p-uptime-rb","children"),
+        Output("p-board-name","children"),
+        Output("p-architecture-name","children"),
+        Output("p-build-time","children"),
+        Output("p-firmware-version","children"),
+        Output("p-bad-blocks","children"),
+        
     ],
     Input('interval-component', 'n_intervals'),
 )
 
 def cb_graduated_bar(n):
     global uso_cpu
-    data_sys = database['rb_sytem'].find().sort('data-hora',-1).limit(1)  # Limitando a 10 registros
+    data_sys = database['rb_sytem'].find().sort('data-hora',-1).limit(1)  
 
     df_sys = pd.DataFrame(data_sys)
 
@@ -212,6 +386,13 @@ def cb_graduated_bar(n):
     cpu_t  = [df_sys.iloc[0]['cpu-temperature']]
     cpu_t2 = df_sys.iloc[0]['cpu-temperature']
 
+    uptime = df_sys.iloc[0]['uptime']
+    board_name = df_sys.iloc[0]['board-name']
+    architecture = df_sys.iloc[0]['architecture-name']
+    build_time = df_sys.iloc[0]['build-time']
+    version = df_sys.iloc[0]['version']
+    bad_blocks = df_sys.iloc[0]['bad-blocks']
+
     uso_cpu = cpu_t2
 
-    return (cpu_l, cpu_f2,f"{cpu_f} Mhz", cpu_t, f"{cpu_t2} ºC")
+    return (cpu_l, cpu_f2,f"{cpu_f} Mhz", cpu_t, f"{cpu_t2} ºC",uptime,board_name,architecture,build_time,version,bad_blocks)
